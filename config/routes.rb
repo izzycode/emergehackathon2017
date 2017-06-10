ef97e9a17c9001127201e69b8449519a91c4a486
izzycode <marcemc@yahoo.com>
2017-06-10 12:50:49 -0400

    remove devise routes and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,4 @@ Rails.application.routes.draw do
   resources :goals
   resources :users
 
-  Rails.application.routes.draw do
-    devise_for :users, controllers: {
-      sessions: 'users/sessions',
-    },
-  end
-
-  
-
 end
