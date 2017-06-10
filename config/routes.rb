a6443aee44e60f99a01e1ac98dfa22dd3ea56695
izzycode <marcemc@yahoo.com>
2017-06-10 17:23:27 -0400

    Integrate instagram sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+
+  root 'home#index'
+
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    omniauth_callbacks: 'users/omniauth_callbacks'
+  }
   resources :goals
   resources :users
-
 end
