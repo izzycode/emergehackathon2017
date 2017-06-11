c0d4eec7801a649a00aae87511f3f672bdd7ee0c
izzycode <marcemc@yahoo.com>
2017-06-10 20:13:03 -0400

    Was in different folder

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170610161554) do
+ActiveRecord::Schema.define(version: 20170610230253) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20170610161554) do
     t.integer  "user_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.datetime "goal_time"
     t.index ["user_id"], name: "index_goals_on_user_id", using: :btree
   end
 
