1f9d54b3d61bdda6c3976a2d8a274113114b7d7f
BuildTools <unconfigured@null.spigotmc.org>
2017-06-11 02:12:48 -0400

    Added a topbar image in app/assets/images and used it in index.html.erb

--- db/schema.rb
+++ db/schema.rb
@@ -53,19 +53,10 @@ ActiveRecord::Schema.define(version: 20170611013226) do
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.string   "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string   "unconfirmed_email"
-    t.integer  "failed_attempts",        default: 0,  null: false
-    t.string   "unlock_token"
-    t.datetime "locked_at"
     t.boolean  "admin"
     t.string   "profile_picture"
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-    t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true, using: :btree
   end
 
   add_foreign_key "goals", "users"
