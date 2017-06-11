12f2043570852fd22c9128722076ee9080f06223
William Bright <williambright91@gmail.com>
2017-06-11 05:23:52 -0400

    fixed image url and render for users

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -3,11 +3,16 @@ class Picture < ApplicationRecord
 
   validates :picture,
   attachment_content_type: { content_type: /\Aimage\/.*\Z/ },
-  attachment_size: { less_than: 5.megabytes }
+  attachment_size: { less_than: 8.megabytes }
 
   has_attached_file :picture, styles: {
-    original: {convert_options: '-auto-orient'}
+    original: {convert_options: '-auto-orient'},
   }
 
+  process_in_background :picture, processing_image_url: :processing_image_fallback, queue: "paperclip"
 
+ def processing_image_fallback
+   options = picture.options
+   options[:interpolator].interpolate(options[:url], picture, :original)
+ end
 end
